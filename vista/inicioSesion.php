15dce9b272c39eefafda82f93e5e557c698228e2
YOLIBET-AL <yoli86_96@hotmail.com>
2024-06-22 20:56:20 -0600

    Update inicioSesion.php

--- vista/inicioSesion.php
+++ vista/inicioSesion.php
@@ -28,7 +28,14 @@
                     </form>
 
                     <!--Register-->
-                   
+                            <form action="" class="formulario__register">
+                                <h2>Regístrarse</h2>
+                                <input type="text" placeholder="Nombre completo">
+                                <input type="text" placeholder="Correo Electronico">
+                                <input type="text" placeholder="Usuario">
+                                <input type="password" placeholder="Contraseña">
+                                <button>Regístrarse</button>
+                            </form>
                 </div>
             </div>
 
