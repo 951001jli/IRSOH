01bf6f1b858428b0ebb099d0df75569c6a69699d
wizzzzzzrd <wizzzzzzrd@gmail.com>
2024-07-01 23:05:24 -0600

    conexion de web esp32 con validacion de inicio de sesion

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -12,7 +12,7 @@ $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE
 
 if(mysqli_num_rows($validar_login) > 0){ 
     $_SESSION['usuario'] = $correo;
-    header ("location: ../bienvenido.php");
+    header ("location: ../ESP32/esp32 cam/pagina.html");
     exit;
 }else{ 
     echo ' 
