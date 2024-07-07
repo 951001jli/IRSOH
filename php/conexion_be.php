7def4b48773ce4dc7c4f9ba98be70fa1e902768f
951001jli <67280579+951001jli@users.noreply.github.com>
2024-07-06 20:13:19 -0600

    se agrega diseño a login

---
+++ php/conexion_be.php
@@ -0,0 +1,14 @@
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "irsho_db");
+/*
+if($conexion){
+echo 'conectado a la base de datos';
+}else{
+    echo 'no se pudo conectar a la base de datos';
+}
+
+
+*/
+
+
+?>
