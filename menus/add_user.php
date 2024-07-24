6b688735437bcf4765bb5ec20ee5dffe2930e7fb
951001jli <67280579+951001jli@users.noreply.github.com>
2024-07-24 17:30:49 -0600

    se corrige diseño de agregar usuario

--- menus/add_user.php
+++ menus/add_user.php
@@ -39,48 +39,49 @@
 ?>
 <?php include_once('layouts/header.php'); ?>
   <?php echo display_msg($msg); ?>
-  <div class="row">
+  
+  <div class="col-md-12">
     <div class="panel panel-default">
-    <div class="panel-heading">
       <div class="panel-heading">
-        <strong>
-          <span class="glyphicon glyphicon-th"></span>
-          <span>Agregar usuario</span>
-       </strong>
-      </div>
-</div>
-      <div class="panel-body">
-        <div class="col-md-6">
-          <form method="post" action="add_user.php">
-            <div class="form-group">
-                <label for="name">Nombre</label>
-                <input type="text" class="form-control" name="full-name" placeholder="Nombre completo" required>
+        <div class="panel-heading clearfix">
+          
+                        <strong>
+                            <span class="glyphicon glyphicon-th"></span>
+                            <span>Agregar usuario</span>
+                        </strong>
+                    </div>
+                    </div>
+                    <div class="panel-body">
+                        <form method="post" action="add_user.php">
+                            <div class="form-group mb-3">
+                                <label for="name">Nombre</label>
+                                <input type="text" class="form-control" name="full-name" placeholder="Nombre completo" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="username">Usuario</label>
+                                <input type="text" class="form-control" name="username" placeholder="Nombre de usuario">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="password">Contraseña</label>
+                                <input type="password" class="form-control" name="password" placeholder="Contraseña">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="level">Rol de usuario</label>
+                                <select class="form-control" name="level">
+                                    <?php foreach ($groups as $group ):?>
+                                    <option value="<?php echo $group['group_level'];?>"><?php echo ucwords($group['group_name']);?></option>
+                                    <?php endforeach;?>
+                                </select>
+                            </div>
+                            <div class="form-group clearfix">
+                                <button type="submit" name="add_user" class="btn btn-primary">Guardar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="username">Usuario</label>
-                <input type="text" class="form-control" name="username" placeholder="Nombre de usuario">
-            </div>
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" name ="password"  placeholder="Contraseña">
-            </div>
-            <div class="form-group">
-              <label for="level">Rol de usuario</label>
-                <select class="form-control" name="level">
-                  <?php foreach ($groups as $group ):?>
-                   <option value="<?php echo $group['group_level'];?>"><?php echo ucwords($group['group_name']);?></option>
-                <?php endforeach;?>
-                </select>
-            </div>
-            <div class="form-group clearfix">
-              <button type="submit" name="add_user" class="btn btn-primary">Guardar</button>
-            </div>
-        </form>
         </div>
-
-      </div>
-
     </div>
-  </div>
+  
 
 <?php include_once('layouts/footer.php'); ?>
